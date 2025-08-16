753ab1bf47fc328027165bd2721897b078d07973
tanyars159-AI <tanyars159@gmail.com>
2025-08-16 12:48:14 +0530

    just checking

--- apps.py
+++ apps.py
@@ -1,6 +1,6 @@
-from django.apps import AppConfig
+from django.apps import AppConfig#type: ignore
 
 
 class StoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'store'
+    name = 'Shopsy'
